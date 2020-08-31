95cb704e8bcefb7b627d3bf43f468a87172a2eb5
qyefearth <zxw5323@psu.edu>
2020-08-31 01:42:32 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,5 @@
+temperature = input("Enter temperature in celsius: ")
+temperature = int(temperature)
+print(temperature *  9/5 + 32 )
+
+print(str(temperature) + "° in Celsius is equivalent to " + str(temperature *  9/5 + 32 ) + "° Fahrenheit. ")
